7b687f31f7643a4fe6b049f0bd012a832c046001
Eduardo Moraes <eduzinho2118@gmail.com>
2026-02-24 22:27:28 -0300

    add songs.js agora fica tudo mais facil

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,5 @@
 // Dados das músicas - CORRIGIDO: Agora aceita link ou apenas o ID
-const songs = [
-    { 
-        id: '1', title: 'Eu so de Jesus', artist: 'Irmão Lazaro', 
-        youtubeId: 'https://youtu.be/_kxqoo6QInM?si=aZIR7BpMDG-AR6wB', cover: '🎵' 
-    },
-    { 
-        id: '2', title: 'Efesio 6', artist: 'Anderson Freire', 
-        youtubeId: 'https://youtu.be/zJOINygRqtQ?si=zG5kk6LCJMFX__C6', cover: '🎵' 
-    },
-    { 
-        id: '3', title: 'Efesio 6 (Versão Rock)', artist: 'Erion Sounds', 
-        youtubeId: 'https://youtu.be/B_m5vhUjPVA?si=ZaCb-wQngOiBzNmo', cover: '🎵' 
-    },
-    { 
-        id: '4', title: 'Todavia Me Alegrarei', artist: 'Leandro Borges', 
-        youtubeId: '6C0fndA7fV0', cover: '🎵' 
-    },
-    { 
-        id: '5', title: 'Bondade de Deus', artist: 'Isaias Saad', 
-        youtubeId: '6mO4o6_S9u8', cover: '🎵' 
-    },
-    { 
-        id: '6', title: 'Raridade', artist: 'Anderson Freire', 
-        youtubeId: 'm4I9m_9X0vM', cover: '🎵' 
-    },
-    { 
-        id: '7', title: 'Raridade', artist: 'Anderson Freire', 
-        youtubeId: 'm4I9m_9X0vM', cover: '🎵' 
-    },
-    { 
-        id: '8', title: 'Raridade', artist: 'Anderson Freire', 
-        youtubeId: 'm4I9m_9X0vM', cover: '🎵' 
-    },
-    { 
-        id: '9', title: 'Raridade', artist: 'Anderson Freire', 
-        youtubeId: 'm4I9m_9X0vM', cover: '🎵' 
-    },
-    { 
-        id: '10', title: 'Raridade', artist: 'Anderson Freire', 
-        youtubeId: 'm4I9m_9X0vM', cover: '🎵' 
-    },
-];
+import { songs } from "./songs.js";
 
 // Inicialização
 document.addEventListener('DOMContentLoaded', () => {
@@ -120,3 +79,6 @@ if (searchInput) {
         renderSongs('all-songs-grid', filtered);
     });
 }
+
+window.playVideo = playVideo;
+window.showPage = showPage;
